59b4a4304cc6b631df431b557cd781346d4a20cd
Kawee Lertrungmongkol <lertrungmongkol@gmail.com>
2020-01-02 10:58:47 +0700

    initial

---
+++ src/main/java/com/simple/rookie/RookieApplication.java
@@ -0,0 +1,13 @@
+package com.simple.rookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RookieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simple/rookie/RookieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.rookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RookieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
